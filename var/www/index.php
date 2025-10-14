7ad483d192aaab4cab178e83334a199930695a44
N0x41 <231366005+N0x41@users.noreply.github.com>
2025-10-14 22:08:15 +0200

    fix: default php config

--- var/www/index.php
+++ var/www/index.php
@@ -4,13 +4,17 @@
 <title>Welcome to nginx!</title>
 <style>
     body {
-        width: 35em;
         margin: 0 auto;
         font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+    .nginx {
+        width: 35em;
+        margin: 0 auto;
     }
 </style>
 </head>
 <body>
+    <div class="nginx">
 <h1>Welcome to nginx!</h1>
 <p>If you see this page, the nginx web server is successfully installed and
 working. Further configuration is required.</p>
@@ -21,5 +25,10 @@ Commercial support is available at
 <a href="http://nginx.com/">nginx.com</a>.</p>
 
 <p><em>Thank you for using nginx.</em></p>
+</div>
+<?php
+
+phpinfo(INFO_MODULES);
+?>
 </body>
 </html>
