b82d6d1f50f7931c0f955120e3ad6d7fb206fc6e
N0x41 <231366005+N0x41@users.noreply.github.com>
2025-10-15 17:20:08 +0200

    fix: bad type for fs.delete

--- NiMP/utils/fs.py
+++ NiMP/utils/fs.py
@@ -1,7 +1,7 @@
 import pathlib
 
-def delete(*path: pathlib.StrPath):
-    mysql_sock_path = pathlib.Path.joinpath(pathlib.Path.cwd(), path);
+def delete(*path: str):
+    mysql_sock_path = pathlib.Path.joinpath(pathlib.Path.cwd(), *path);
     if pathlib.Path.exists(mysql_sock_path, follow_symlinks=False):
         try:
             pathlib.Path.unlink(mysql_sock_path)
